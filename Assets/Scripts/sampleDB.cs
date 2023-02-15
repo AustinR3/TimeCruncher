66d9770fef5f67e0eb83434074d2aef0b2ca8d5d
rsandhuOU <97359092+rsandhuOU@users.noreply.github.com>
2023-02-15 11:24:10 -0500

    database modification

--- Assets/Scripts/sampleDB.cs
+++ Assets/Scripts/sampleDB.cs
@@ -19,20 +19,20 @@ public class sampleDB : MonoBehaviour {
 		// Create table
 		IDbCommand dbcmd;
 		dbcmd = dbcon.CreateCommand();
-		string q_createTable = "CREATE TABLE IF NOT EXISTS level_table1 (id INTEGER PRIMARY KEY, val INTEGER )";
+		string q_createTable = "CREATE TABLE IF NOT EXISTS table1 (id INTEGER PRIMARY KEY, val INTEGER )";
 		
 		dbcmd.CommandText = q_createTable;
 		dbcmd.ExecuteReader();
 
 		// Insert values in table
 		IDbCommand cmnd = dbcon.CreateCommand();
-		cmnd.CommandText = "INSERT or IGNORE INTO level_table1 (id, val) VALUES (100, 1), (101, 2), (102, 3), (103, 4)";
+		cmnd.CommandText = "INSERT or IGNORE INTO table1 (id, val) VALUES (100, 1), (101, 2), (102, 3), (103, 4)";
 		cmnd.ExecuteNonQuery();
 
 		// Read and print all values in table
 		IDbCommand cmnd_read = dbcon.CreateCommand();
 		IDataReader reader;
-		string query ="SELECT * FROM level_table1";
+		string query ="SELECT * FROM table1";
 		cmnd_read.CommandText = query;
 		reader = cmnd_read.ExecuteReader();
 
