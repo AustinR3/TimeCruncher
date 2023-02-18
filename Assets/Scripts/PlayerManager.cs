272ffa3e88154457f481bc6dc1196c0afc946db3
Austin Radhs <austinradhs@oakland.edu>
2023-02-18 09:00:23 -0500

    Level 2

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     // Start is called before the first frame update
-
+public static Vector2 lastCheckPointPos = new Vector2(-3,0);
 public GameObject[] playerPrefabs;
 int characterIndex;
 
 private void Awake()
 {
     characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
-    Instantiate(playerPrefabs[characterIndex]);
+    GameObject player =  Instantiate(playerPrefabs[characterIndex], lastCheckPointPos, Quaternion.identity);
+    //Instantiate(playerPrefabs[characterIndex], Quaternion.identity);
 
 
 
