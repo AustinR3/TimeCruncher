53587f713b1eb38eb405b7f87ba45889a9218cdb
Austin Radhs <austinradhs@oakland.edu>
2023-03-11 09:49:46 -0500

    Button Hover & Click AUDIO

--- Assets/Scripts/btnFX.cs
+++ Assets/Scripts/btnFX.cs
@@ -6,11 +6,27 @@ public class btnFX : MonoBehaviour
 {
 
 
+public AudioSource myFx;
+public AudioClip hoverFx;
+public AudioClip clickFx; 
 
 
 
+public void HoverSound ()
+{
 
 
+myFx.PlayOneShot (hoverFx);
+
+}
+
+public void ClickSound ()
+{
+
+myFx.PlayOneShot (clickFx);
+
+
+}
 
 
 
